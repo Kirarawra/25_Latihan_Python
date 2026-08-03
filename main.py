600ec20e90f07efab303387146086219861e118f
Kira <rizqi.alrayyan@gmail.com>
2026-08-03 13:45:03 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+angka = int(input("Masukkan sebuah bilangan :"))
+
+if angka % 2 == 0:
+    print("Bilangan", angka, "adalah Genap")
+else:
+    print("Bilangan", angka, "adalah Ganjil")
---
+++ main1.py
@@ -0,0 +1,6 @@
+p = int(input("Panjang persegi panjang :"))
+l = int(input("Luas persegi panjang : "))
+
+a = p*l 
+
+print(a) 
